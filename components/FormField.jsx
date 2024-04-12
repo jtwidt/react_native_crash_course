502e827fae88c71e0a343a69842ff15d557a810f
Jeremy Twidt <jtwidt@gmail.com>
2024-04-12 17:34:01 -0500

    auth screens complete

---
+++ components/FormField.jsx
@@ -0,0 +1,46 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
+
+import { icons } from '../constants';
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  keyboardType,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>
+
+      <View className=' flex-row w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center border-2 border-black-200'>
+        <TextInput
+          className='flex-1 text-white font-psemibold text-base'
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor='#7B7B8B'
+          onChangeText={handleChangeText}
+          keyboardType={keyboardType ? keyboardType : 'default'}
+          secureTextEntry={title === 'Password' && !showPassword}
+        />
+
+        {title === 'Password' && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className='w-6 h-6'
+              resizeMode='contain'
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default FormField;
