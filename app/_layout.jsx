2e91549090839827fed6554eda0a9e234d555819
Jeremy Twidt <jtwidt@gmail.com>
2024-04-13 08:54:23 -0500

    global context created

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { SplashScreen, Stack } from 'expo-router';
 import { useFonts } from 'expo-font';
 
+import GlobalProvider from '../context/GlobalProvider';
+
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
@@ -26,12 +28,14 @@ const RootLayout = () => {
   }, [fontsLoaded, error]);
 
   return (
-    <Stack>
-      <Stack.Screen name='index' options={{ headerShown: false }} />
-      <Stack.Screen name='(auth)' options={{ headerShown: false }} />
-      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-      {/* <Stack.Screen name='/search/[query]' options={{ headerShown: false }} /> */}
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name='index' options={{ headerShown: false }} />
+        <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        {/* <Stack.Screen name='/search/[query]' options={{ headerShown: false }} /> */}
+      </Stack>
+    </GlobalProvider>
   );
 };
 
